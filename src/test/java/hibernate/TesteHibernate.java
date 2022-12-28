51a37e1b15c87b3ca84c71d2a7935a7107d3cda2
Pelegrino <pelegrino@gmail.com>
2022-12-28 11:00:20 -0300

    Parâmetros e queryes condicionais

--- src/test/java/hibernate/TesteHibernate.java
+++ src/test/java/hibernate/TesteHibernate.java
@@ -105,4 +105,16 @@ public class TesteHibernate {
 		}
 	}
 	
+	@Test
+	public void testeQueryListParameter() {
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+
+		List<UsuarioPessoa> list = daoGeneric.getEntityManager().createQuery("from UsuarioPessoa where nome = :nome and sobrenome = :sobrenome").setParameter("nome", "Rafaella").setParameter("sobrenome","Pelegrino").getResultList();
+		
+		for (UsuarioPessoa usuarioPessoa : list) {
+			System.out.println(usuarioPessoa);
+			
+		}
+	}
+	
 }
