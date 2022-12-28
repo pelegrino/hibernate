625e2eb310b31090264a6e691164ffac7e62db07
Pelegrino <pelegrino@gmail.com>
2022-12-28 11:07:22 -0300

    Efetuando operações matemáticas

--- src/test/java/hibernate/TesteHibernate.java
+++ src/test/java/hibernate/TesteHibernate.java
@@ -117,4 +117,14 @@ public class TesteHibernate {
 		}
 	}
 	
+	@Test
+	public void testeQuerySomaIdade() {
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+
+		Long somaIdade = (Long) daoGeneric.getEntityManager().createQuery("select sum(u.idade) from UsuarioPessoa u").getSingleResult();
+	
+		System.out.println("Soma de todas as idades é: " + somaIdade);
+	}
+	
+	
 }
