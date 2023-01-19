2c4e6e85584ef8d1aaa3df60d2fd94851c244e33
Pelegrino <pelegrino@gmail.com>
2023-01-19 15:19:38 -0300

    Carregando dados na tela

---
+++ src/main/java/managedBean/UsuarioPessoaManagedBean.java
@@ -0,0 +1,53 @@
+package managedBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
+@ManagedBean(name="usuarioPessoaManagedBean")
+@ViewScoped
+public class UsuarioPessoaManagedBean {
+
+	
+	private UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
+	private DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<>();
+	private List<UsuarioPessoa> list = new ArrayList<UsuarioPessoa>();
+	
+	
+
+	//Getters e Setters
+	
+	public UsuarioPessoa getUsuarioPessoa() {
+		return usuarioPessoa;
+	}
+	
+	public void setUsuarioPessoa(UsuarioPessoa usuarioPessoa) {
+		this.usuarioPessoa = usuarioPessoa;
+	}
+	
+	public List<UsuarioPessoa> getList() {
+		list = daoGeneric.listar(UsuarioPessoa.class);
+		return list;
+	}
+	
+	
+	
+	//Metodos
+	
+	public String salvar() {
+		daoGeneric.salvar(usuarioPessoa);
+		
+		return "";
+	}
+	
+	public String novo() {
+		usuarioPessoa = new UsuarioPessoa();
+		return "";
+	}
+	
+}
