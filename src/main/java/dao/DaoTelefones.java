7a50d036c281941c4865304b4a7a994c79f31784
Pelegrino <pelegrino@gmail.com>
2023-01-20 11:23:49 -0300

    Gravando telefones para o usuário

---
+++ src/main/java/dao/DaoTelefones.java
@@ -0,0 +1,7 @@
+package dao;
+
+import model.TelefoneUser;
+
+public class DaoTelefones<E> extends DaoGeneric<TelefoneUser> {
+
+}
--- src/main/java/managedBean/TelefoneManagedBean.java
+++ src/main/java/managedBean/TelefoneManagedBean.java
@@ -1,11 +1,14 @@
 package managedBean;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
+import dao.DaoTelefones;
 import dao.DaoUsuario;
+import model.TelefoneUser;
 import model.UsuarioPessoa;
 
 @ManagedBean(name = "telefoneManagedBean")
@@ -15,6 +18,8 @@ public class TelefoneManagedBean {
 	
 	public UsuarioPessoa user = new UsuarioPessoa();
 	private DaoUsuario<UsuarioPessoa> daoUser = new DaoUsuario<UsuarioPessoa>();
+	private DaoTelefones<TelefoneUser> daoTelefones = new DaoTelefones<TelefoneUser>();
+	private TelefoneUser telefone = new TelefoneUser();
 	
 
 	@PostConstruct
@@ -24,12 +29,37 @@ public class TelefoneManagedBean {
 		user = daoUser.pesquisar(Long.parseLong(coduser), UsuarioPessoa.class);
 	}
 	
+	public String salvar() {
+		telefone.setUsuarioPessoa(user);
+		daoTelefones.salvar(telefone);
+		telefone = new TelefoneUser();
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Informação", "Salvo com sucesso"));
+		return "";
+	}
+	
+	
 	public UsuarioPessoa getUser() {
 		return user;
 	}
 	
 	public void setUser(UsuarioPessoa user) {
 		this.user = user;
+	}
+	
+	public DaoTelefones<TelefoneUser> getDaoTelefones() {
+		return daoTelefones;
+	}
+	
+	public void setDaoTelefones(DaoTelefones<TelefoneUser> daoTelefones) {
+		this.daoTelefones = daoTelefones;
+	}
+	
+	public void setTelefone(TelefoneUser telefone) {
+		this.telefone = telefone;
+	}
+	
+	public TelefoneUser getTelefone() {
+		return telefone;
 	}
 	
 }
