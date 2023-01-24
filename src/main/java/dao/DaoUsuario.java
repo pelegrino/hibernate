2983ea6fbdf7b11bfdf7ae2d9e0c8ed85a580500
Pelegrino <pelegrino@gmail.com>
2023-01-24 08:59:14 -0300

    Removendo emails e telefones em cascata

--- src/main/java/dao/DaoUsuario.java
+++ src/main/java/dao/DaoUsuario.java
@@ -8,8 +8,17 @@ public class DaoUsuario<E> extends DaoGeneric<UsuarioPessoa> {
 	
 	public void removerUsuario(UsuarioPessoa pessoa) throws Exception {
 		getEntityManager().getTransaction().begin();
+		
+		/*
 		String sqlDeleteFone = "delete from telefoneuser where usuariopessoa_id = " + pessoa.getId();
 		getEntityManager().createNativeQuery(sqlDeleteFone).executeUpdate();
+		
+		sqlDeleteFone = "delete from emailuser where usuariopessoa_id = " + pessoa.getId();
+		getEntityManager().createNativeQuery(sqlDeleteFone).executeUpdate();
+		 */
+		
+		getEntityManager().remove(pessoa);
+		
 		getEntityManager().getTransaction().commit();
 		
 		super.deletarPorId(pessoa);
